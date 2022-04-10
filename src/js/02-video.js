a0be578ae5cfb20d0d97fd167e7cafaeb6c87036
Anton Pavlenko <86660391+antohapavlenko@users.noreply.github.com>
2022-04-10 20:27:06 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    
+
+    player.on('timeupdate', throttle((function(event) {
+        console.log('played the video!');
+        localStorage.setItem("videoplayer-current-time", event.seconds)
+    }), 3000)
+    );
+
+
+    
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+    });
+
+
+    
+
+   
+
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const FORM_KEY = "feedback-form-state";
+
+// Долдаємо дані з форми в сховище
+feedbackForm.addEventListener('input', throttle(() => {
+    const formData = new FormData(feedbackForm)
+    let getData = {};
+    formData.forEach((value, key) =>  getData[key] = value.trim());
+    localStorage.setItem(FORM_KEY, JSON.stringify(getData))
+    
+},500));
+
+// Отдимуємо дані з локального сховища
+const useData = JSON.parse(localStorage.getItem(FORM_KEY))
+// Перетворюємо обєкт в масив та вставляємо дані в поля форми
+if (useData) {
+    Object.entries(useData).forEach(([key, value]) => feedbackForm.elements[key].value = value);
+}
+
+// відправляємо форму та очищюємо формі і локал сторедж
+feedbackForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log(useData)
+    localStorage.clear()
+    event.currentTarget.reset()
+})
 
